646f530bb29816df018c70e3dc9bb920c4c2e4d3
kkcm <koloczek@student.agh.edu.pl>
2019-06-02 00:39:17 +0200

    [lab3] treeprinter add -> program, whileloop, condition, printintented.

--- AST.py
+++ AST.py
@@ -7,17 +7,13 @@ class IntNum(Node):
     def __init__(self, value):
         self.value = value
 
-
 class FloatNum(Node):
-
     def __init__(self, value):
         self.value = value
-
 
 class Variable(Node):
     def __init__(self, name):
         self.name = name
-
 
 class BinExpr(Node):
     def __init__(self, op, left, right):
--- treeprinter.py
+++ treeprinter.py
@@ -10,15 +10,34 @@ def addToClass(cls):
 
 class TreePrinter:
 
+    @classmethod
+    def printIndented(cls, string, level):
+        print ("|   " * level + string)
+
     @addToClass(AST.Node)
     def printTree(self, indent=0):
         raise Exception("printTree not defined in class " + self.__class__.__name__)
-
 
     @addToClass(AST.IntNum)
     def printTree(self, indent=0):
         TreePrinter.printIndented(str(self.value), indent)
 
+    @addToClass(AST.Program)
+    def printTree(self, indent=0):
+        for instruction in self.instruction_lines:
+            instruction.printTree(indent)
+
+    @addToClass(AST.WhileLoop)
+    def printTree(self, indent=0):
+        TreePrinter.printIndented('WHILE', indent)
+        self.condition.printTree(indent + 1)
+        self.instruction_lines.printTree(indent + 1)
+
+    @addToClass(AST.Condition)
+    def printTree(self, indent=0):
+        TreePrinter.printIndented(self.op, indent)
+        self.left.printTree(indent + 1)
+        self.right.printTree(indent + 1)
 
     @addToClass(AST.Error)
     def printTree(self, indent=0):
