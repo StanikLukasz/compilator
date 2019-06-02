b80f7377a5b088c079d0f42137fc20c22b13e560
kkcm <koloczek@student.agh.edu.pl>
2019-06-02 19:10:30 +0200

    [lab4] symboltable - add some methods.

--- symboltable.py
+++ symboltable.py
@@ -1,37 +1,46 @@
 #!/usr/bin/python
-from symtable import Symbol
 
+
+class Symbol(object):
+    pass
 
 class VariableSymbol(Symbol):
 
-    def __init__(self, name, type):
-        pass
-    #
+    def __init__(self, name, type, value=None):
+        self.name = name
+        self.type = type
+        self.value = value
 
 
 class SymbolTable(object):
 
     def __init__(self, parent, name): # parent scope and symbol table name
-        pass
-    #
+        self.symbols = {}
+        self.parent = parent
+        self.name = name
 
     def put(self, name, symbol): # put variable symbol or fundef under <name> entry
-        pass
-    #
+        self.symbols[name] = symbol
+
 
     def get(self, name): # get variable symbol or fundef from <name> entry
-        pass
-    #
+        if self.symbols.__contains__(name):
+            return self.symbols[name]
+        elif self.parent:
+            return self.parent.get(name)
+        else:
+            return None
+
 
     def getParentScope(self):
-        pass
-    #
+        return self.parent
+
 
     def pushScope(self, name):
         pass
-    #
+
 
     def popScope(self):
         pass
-    #
+
 
