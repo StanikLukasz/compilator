ab2704e25f43b61f898ddf4ef273c57f6920aff2
TKMA09F <lukasz.stanik@kohls.com>
2019-06-02 19:53:37 +0200

    [lab4] Mock visit methods in typechecker.py

--- typechecker.py
+++ typechecker.py
@@ -7,7 +7,7 @@ class NodeVisitor(object):
 
     def visit(self, node):
         method = 'visit_' + node.__class__.__name__
-        visitor = getattr(self, method, self.generic_visit)
+        visitor = getattr(self, method, self.generic_visit) # visitor = self.method - default = self.generic_visit
         return visitor(node)
 
     def generic_visit(self, node):  # Called if no explicit visitor function exists for a node.
@@ -31,6 +31,84 @@ class NodeVisitor(object):
 
 class TypeChecker(NodeVisitor):
 
+# 0. Base Node class
+
+    def visit_Node(self, node):
+        pass
+
+
+## 1. Initial rules
+
+    def visit_Program(self, node):
+        pass
+
+    def visit_Empty(self, node):
+        pass
+
+
+## 2. Statement types
+
+    def visit_IfElse(self, node):
+        pass
+
+    def visit_WhileLoop(self, node):
+        pass
+
+    def visit_ForLoop(self, node):
+        pass
+
+    def visit_CodeBlock(self, node):
+        pass
+
+# 2.1 Condition statements
+    def visit_Condition(self, node):
+        pass
+
+# 2.2 Values range
+    def visit_Range(self, node):
+        pass
+
+
+## 3. Instruction types
+
+    def visit_Assignment(self, node):
+        pass
+
+    def visit_Print(self, node):
+        pass
+
+    def visit_Continue(self, node):
+        pass
+
+    def visit_Break(self, node):
+        pass
+
+    def visit_Return(self, node):
+        pass
+
+# 3.1 Assignment operators
+# no AST classes for this part
+
+
+## 4. Expressions
+
+# 4.1 Array definition (and array lines used in other structures)
+    def visit_Array(self, node):
+        pass
+
+# 4.2 Array special functions
+    def visit_Function(self, node):
+        pass
+
+# 4.3 Array transposition
+    def visit_Transposition(self, node):
+        pass
+
+# 4.4 Unary negation
+    def visit_Negation(self, node):
+        pass
+
+# 4.5 Binary expressions
     def visit_BinExpr(self, node):
         # alternative usage,
         # requires definition of accept method in class Node
@@ -40,7 +118,27 @@ class TypeChecker(NodeVisitor):
         # ...
         #
 
-    def visit_Variable(self, node):
+# 4.6 Expressions in parenthesis
+# no AST classes for this part
+
+# 4.7 Elementary types
+    def visit_String(self, node):
+        pass
+
+    def visit_Identifier(self, node):
+        pass
+
+    def visit_Reference(self, node):
+        pass
+
+    def visit_Integer(self, node):
+        pass
+
+    def visit_Real(self, node):
         pass
 
 
+# X. Error raising
+
+    def visit_Error(self, node):
+        pass
