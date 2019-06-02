93825aa7f430830b1a178bc3a2570caefd1fb1d5
kkcm <koloczek@student.agh.edu.pl>
2019-06-02 23:56:03 +0200

    [lab5] initial commit.

--- main_interpreter.py
+++ main_interpreter.py
@@ -4,11 +4,12 @@ import scanner
 import parser
 import treeprinter
 import typechecker
+import interpreter
 
 if __name__ == '__main__':
 
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "examples/example4_4.m"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "example.txt"
         file = open(filename, "r")
     except IOError:
         print("Cannot open {0} file".format(filename))
@@ -27,3 +28,11 @@ if __name__ == '__main__':
         typeChecker.visit(ast)
     except ValueError as e:
          print(e)
+
+    # ast.accept(Interpreter())
+    # in future
+    # ast.accept(OptimizationPass1())
+    # ast.accept(OptimizationPass2())
+    # ast.accept(CodeGenerator())
+
+
