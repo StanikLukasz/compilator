df62ce968d1090271afb84876ed4f5d4457c2adb
TKMA09F <lukasz.stanik@kohls.com>
2019-06-02 18:36:56 +0200

    [enhc] parser.py organizing

--- parser.py
+++ parser.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python
 
+## TODO:
+# zeros(x,y)
+# ones(x,y)
+
+## Table of contents
+#
+# Error handling
+# 1. Initial rules
+# 2. Statement types
+#   2.1 Condition statements
+#   2.2 Values range
+# 3. Instruction types
+#   3.1 Assignment operators
+# 4. Expressions
+#   4.1 Array definition (and array line used in other structures)
+#   4.2 Array special functions
+#   4.3 Array transposition
+#   4.4 Unary negation
+#   4.5 Binary expressions
+#   4.6 Expressions in parenthesis
+#   4.7 Elementary types
+# Parser building
+
 import scanner
 import ply.yacc as yacc
 import AST
@@ -19,7 +42,7 @@ precedence = (
     ("nonassoc", "'")
 )
 
-
+# Error handling
 def p_error(p):
     if p:
         raise ValueError("Syntax error at line {0}, column {1}: LexToken({2}, '{3}')".format(p.lineno, s.find_tok_column(p), p.type, p.value))
@@ -27,6 +50,7 @@ def p_error(p):
         raise EOFError("Unexpected end of input")
 
 
+## 1. Initial rules
 def p_program(p):
     """program : instruction_lines
                | empty"""
@@ -45,12 +69,13 @@ def p_instructions(p):
         p[0] = p[1] + [p[2]]
 
 
+## 2. Statement types
 def p_instruction_line(p):
-    """instruction_line : instruction ';'
-                        | if_else
+    """instruction_line : if_else
                         | while_loop
                         | for_loop
-                        | code_block"""
+                        | code_block
+                        | instruction ';'"""
     p[0] = p[1]
 
 def p_if_else(p):
@@ -73,51 +98,7 @@ def p_code_block(p):
     """code_block : '{' program '}' """
     p[0] = AST.CodeBlock(p[2])
 
-def p_instruction(p):
-    """instruction : assignment
-                   | printing
-                   | continue_statement
-                   | break_statement
-                   | returning"""
-    p[0] = p[1]
-
-def p_continue_statement(p):
-    """continue_statement : CONTINUE"""
-    p[0] = AST.Continue()
-
-def p_break_statement(p):
-    """break_statement : BREAK"""
-    p[0] = AST.Break()
-
-
-# INSTRUCTION TYPES
-
-def p_assignment(p):
-    """assignment : identifier assignment_op expression"""
-    p[0] = AST.Assignment(p[2], p[1], p[3])
-
-def p_printing(p):
-    """printing : PRINT array_line"""
-    p[0] = AST.Print(p[2])
-
-def p_returning(p):
-    """returning : RETURN expression"""
-    p[0] = AST.Return(p[2])
-
-
-# ASSIGNMENT OPEARATORS
-
-def p_assignment_op(p):
-    """assignment_op : '='
-                     | ADDASSIGN
-                     | SUBASSIGN
-                     | MULASSIGN
-                     | DIVASSIGN"""
-    p[0] = p[1]
-
-
-# CONDITION FOR "WHILE" AND "IF" CONSTRUCTIONS
-
+# 2.1 Condition statements
 def p_condition(p):
     """condition : '(' bool_expression ')'"""
     p[0] = p[2]
@@ -135,29 +116,66 @@ def p_comparison_op(p):
                      | LEQ"""
     p[0] = p[1]
 
-
-# NEEDED FOR LOOPS AND "IF" STATEMENT
-
+# 2.2 Values range
 def p_range(p):
     """range : id_or_number ':' id_or_number"""
     p[0] = AST.Range(p[1], p[3])
 
 
-# EXPRESSION DEFINITION
+## 3. Instruction types
+
+def p_instruction(p):
+    """instruction : assignment
+                   | printing
+                   | continue_statement
+                   | break_statement
+                   | returning"""
+    p[0] = p[1]
+
+def p_assignment(p):
+    """assignment : identifier assignment_op expression"""
+    p[0] = AST.Assignment(p[2], p[1], p[3])
+
+def p_printing(p):
+    """printing : PRINT array_line"""
+    p[0] = AST.Print(p[2])
+
+def p_continue_statement(p):
+    """continue_statement : CONTINUE"""
+    p[0] = AST.Continue()
+
+def p_break_statement(p):
+    """break_statement : BREAK"""
+    p[0] = AST.Break()
+
+def p_returning(p):
+    """returning : RETURN expression"""
+    p[0] = AST.Return(p[2])
+
+# 3.1 Assignment operators
+def p_assignment_op(p):
+    """assignment_op : '='
+                     | ADDASSIGN
+                     | SUBASSIGN
+                     | MULASSIGN
+                     | DIVASSIGN"""
+    p[0] = p[1]
+
+
+## 4. Expressions
 
 def p_expression(p):
     """expression : expression_binary
                   | array
-                  | elementary
                   | array_special
-                  | negation
                   | transposition
-                  | expression_group"""
+                  | negation
+                  | expression_group
+                  | elementary"""
     p[0] = p[1]
 
 
-# ARRAY DEFINITION
-
+# 4.1 Array definition (and array lines used in other structures)
 def p_array(p):
     """array : '[' array_lines ']' """
     p[0] = AST.Array(p[2])
@@ -178,72 +196,29 @@ def p_array_line(p):
     else:
         p[0] = p[1] + [p[3]]
 
-
-# ARRAY SPECIAL
-
+# 4.2 Array special functions
 def p_array_special(p):
     """array_special : ZEROS array_special_specifier
                      | ONES array_special_specifier
                      | EYE array_special_specifier """
     p[0] = AST.Function(p[1], [p[2]])
 
-
 def p_array_special_specifier(p):
     """array_special_specifier : '(' id_or_number ')'"""
     p[0] = p[2]
 
+# 4.3 Array transposition
 def p_transposition(p):
     """transposition : identifier "'"
                      | array "'" """
     p[0] = AST.Transposition(p[1])
 
-
-# NEGATION
-
+# 4.4 Unary negation
 def p_negation(p):
     """negation : '-' expression"""
     p[0] = AST.Negation(p[2])
 
-
-# ELEMTARY TYPES
-
-def p_elementary(p):
-    """elementary : id_or_number
-                  | text"""
-    p[0] = p[1]
-
-def p_text(p):
-    """text : STRING"""
-    p[0] = AST.String(p[1])
-
-def p_id_or_number(p):
-    """id_or_number : identifier
-                    | number"""
-    p[0] = p[1]
-
-def p_identifier(p):
-    """identifier : ID '[' array_line ']'
-                  | ID"""
-    if len(p) == 2:
-        p[0] = AST.Identifier(p[1])
-    else:
-        p[0] = AST.Reference(p[1], p[3])
-
-def p_number(p):
-    """number : integer
-              | real"""
-    p[0] = p[1]
-
-def p_integer(p):
-    """integer : INTNUM"""
-    p[0] = AST.Integer(p[1])
-
-def p_real(p):
-    """real : REALNUM"""
-    p[0] = AST.Real(p[1])
-
-# BINARY EXPRESSIONS
-
+# 4.5 Binary expressions
 def p_expression_binary(p):
     """expression_binary : normal_binary_expression
                          | dot_binary_expression"""
@@ -295,10 +270,46 @@ def p_dot_div_expression(p):
     """dot_div_expression : expression DOTDIV expression"""
     p[0] = AST.BinExpr(p[2], p[1], p[3])
 
-# PARENTHESIS
-
+# 4.6 Expressions in parenthesis
 def p_expression_group(p):
     """expression_group : '(' expression ')' """
     p[0] = p[1]
 
+# 4.7 Elementary types
+def p_elementary(p):
+    """elementary : id_or_number
+                  | text"""
+    p[0] = p[1]
+
+def p_text(p):
+    """text : STRING"""
+    p[0] = AST.String(p[1])
+
+def p_id_or_number(p):
+    """id_or_number : identifier
+                    | number"""
+    p[0] = p[1]
+
+def p_identifier(p):
+    """identifier : ID '[' array_line ']'
+                  | ID"""
+    if len(p) == 2:
+        p[0] = AST.Identifier(p[1])
+    else:
+        p[0] = AST.Reference(p[1], p[3])
+
+def p_number(p):
+    """number : integer
+              | real"""
+    p[0] = p[1]
+
+def p_integer(p):
+    """integer : INTNUM"""
+    p[0] = AST.Integer(p[1])
+
+def p_real(p):
+    """real : REALNUM"""
+    p[0] = AST.Real(p[1])
+
+# Parser building
 parser = yacc.yacc()
